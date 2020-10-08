dd13f89bd74558de9245b9e6dca522dfe18c660e
steveush <steveush@gmail.com>
2020-10-08 03:06:13 +0200

    Updated the heading field to support tooltips as well.

--- includes/admin/foofields/fields/class-header.php
+++ includes/admin/foofields/fields/class-header.php
@@ -16,7 +16,9 @@ if ( ! class_exists( __NAMESPACE__ . '\Header' ) ) {
 
 		function render_input( $override_attributes = false ) {
 			if ( isset( $this->label ) ) {
-				self::render_html_tag( 'h3', array(), $this->label );
+				self::render_html_tag( 'h3', array(), $this->label, false );
+				$this->render_tooltip();
+				echo '</h3>';
 			}
 			if ( isset( $this->description ) ) {
 				self::render_html_tag( 'p', array(), $this->description );
